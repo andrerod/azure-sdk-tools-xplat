4e4cb76bda0a2d80a774be5a50f59776d7fa1674
Andre Rodrigues <andrerod@microsoft.com>
2013-07-25 18:39:53 +0100

    Fix

--- test/commands/site.connectionstring-tests.js
+++ test/commands/site.connectionstring-tests.js
@@ -15,12 +15,11 @@
 
 var should = require('should');
 
-var executeCommand = require('../framework/cli-executor').execute;
-var MockedTestUtils = require('../framework/mocked-test-utils');
+var CLITest = require('../framework/cli-test');
 
 var createdSites = [];
 
-var suiteUtil;
+var suite;
 var testPrefix = 'site.connectionstring-tests';
 
 var siteNamePrefix = 'contests';
@@ -29,39 +28,30 @@ var siteNames = [];
 var location = process.env.AZURE_SITE_TEST_LOCATION || 'East US';
 var connectionString = 'Endpoint=sb://gongchen1.servicebus.windows.net/;SharedSecretIssuer=owner;SharedSecretValue=fake=';
 
-var executeCmd = function (cmd, callback) {
-  if (suiteUtil.isMocked && !suiteUtil.isRecording) {
-    cmd.push('-s');
-    cmd.push(process.env.AZURE_SUBSCRIPTION_ID);
-  }
-
-  executeCommand(cmd, callback);
-};
-
 describe('cli', function(){
   describe('site connectionstring', function() {
 
     before(function (done) {
-      suiteUtil = new MockedTestUtils(testPrefix);
-      suiteUtil.setupSuite(done);
+      suite = new CLITest(testPrefix);
+      suite.setupSuite(done);
     });
 
     after(function (done) {
-      suiteUtil.teardownSuite(done);
+      suite.teardownSuite(done);
     });
 
     beforeEach(function (done) {
-      suiteUtil.setupTest(done);
+      suite.setupTest(done);
     });
 
     afterEach(function (done) {
       function removeSite() {
         if (createdSites.length === 0) {
-          return suiteUtil.teardownTest(done);
+          return suite.teardownTest(done);
         }
 
         var siteName = createdSites.pop();
-        executeCmd('site delete %s --json --quiet', siteName, function () {
+        suite.execute('site delete %s --json --quiet', siteName, function () {
           removeSite();
         });
       }
@@ -70,42 +60,35 @@ describe('cli', function(){
     });
 
     it('should list site connectionstring', function(done) {
-      var siteName = suiteUtil.generateId(siteNamePrefix, siteNames);
+      var siteName = suite.generateId(siteNamePrefix, siteNames);
 
       // Create site
-      var cmd = ('node cli.js site create ' + siteName + ' --json --location').split(' ');
-      cmd.push(location);
-      executeCmd(cmd, function (result) {
+      executeCmd('site create %s --json --location %s', siteName, location, function (result) {
         result.text.should.equal('');
         result.exitStatus.should.equal(0);
 
         // List sites
-        cmd = ('node cli.js site connectionstring list ' + siteName + ' --json ').split(' ');
-        executeCmd(cmd, function (result) {
+        executeCmd('site connectionstring list %s --json', siteName, function (result) {
           // there should be not settings yet as the site was just created
           result.exitStatus.should.equal(0);
 
           // add a setting
-          var cmd = ('node cli.js site connectionstring add param1 ' + connectionString + ' SQLAzure ' + siteName + ' --json').split(' ');
-          executeCmd(cmd, function (result) {
+          executeCmd('site connectionstring add param1 %s SQLAzure %s --json', connectionString, siteName, function (result) {
             result.text.should.equal('');
             result.exitStatus.should.equal(0);
 
-            cmd = ('node cli.js site connectionstring list ' + siteName + ' --json').split(' ');
-            executeCmd(cmd, function (result) {
+            executeCmd('site connectionstring list %s --json', siteName, function (result) {
               var settingsList = JSON.parse(result.text);
 
               // Listing should return 1 setting now
               settingsList.length.should.equal(1);
 
               // add another setting
-              var cmd = ('node cli.js site connectionstring add param2 ' + connectionString + ' SQLAzure ' + siteName + ' --json').split(' ');
-              executeCmd(cmd, function (result) {
+              executeCmd('site connectionstring add param2 %s SQLAzure %s --json', connectionString, siteName, function (result) {
                 result.text.should.equal('');
                 result.exitStatus.should.equal(0);
 
-                cmd = ('node cli.js site connectionstring list ' + siteName + ' --json').split(' ');
-                executeCmd(cmd, function (result) {
+                executeCmd('site connectionstring list %s --json', siteName, function (result) {
                   var settingsList = JSON.parse(result.text);
 
                   // Listing should return 2 setting now
@@ -121,7 +104,7 @@ describe('cli', function(){
     });
 
     it('should add get and clear site connectionstring', function(done) {
-      var siteName = suiteUtil.generateId(siteNamePrefix, siteNames);
+      var siteName = suite.generateId(siteNamePrefix, siteNames);
 
       // Create site
       var cmd = ('node cli.js site create ' + siteName + ' --json --location').split(' ');
